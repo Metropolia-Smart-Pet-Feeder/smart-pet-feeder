12fd1fd6569a004d62b414039272bb000fcd8024
Riinaaal <riinaaal@metropolia.fi>
2026-02-15 19:20:55 +0200

    Committing IR sensor component

---
+++ components/IR_sensor/include/IR_sensors.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+#include "esp_log.h"
+
+class IR_sensors {
+    public:
+        explicit IR_sensors(std::string name, gpio_num_t sensor_pin, bool alert_when_high, std::shared_ptr<EventBus> event_bus);
+        ~IR_sensors();
+        void start_monitoring();
+        
+
+    private:
+        static void enter_task(void *arg);
+        void monitor_sensors();
+
+        std::string name;
+        gpio_num_t sensor_pin;
+        bool alert_when_high;
+        std::shared_ptr<EventBus> event_bus;
+        int last_sensor_state;
+};
