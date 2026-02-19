30dd754f3ce61c675bba14429e7255748dba6095
Sheng Tai <shengtai25@outlook.com>
2026-02-19 20:43:25 +0200

    refactor: add new feeding event definitions and introduce new types for feeding and calibration

---
+++ components/common/include/Types.h
@@ -0,0 +1,86 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include "freertos/queue.h"
+
+// Feeding source
+enum feed_source_t : uint8_t
+{
+    FEED_SOURCE_MANUAL    = 0,
+    FEED_SOURCE_SCHEDULED = 1,
+    FEED_SOURCE_REMOTE    = 2
+};
+
+// Feeding status codes
+enum feed_status_code_t : uint8_t
+{
+    FEED_STATUS_SUCCESS        = 0,
+    FEED_STATUS_TIMEOUT        = 1,
+    FEED_STATUS_BUSY           = 2,
+    FEED_STATUS_LOW_FOOD       = 3,
+    FEED_STATUS_UNKNOWN        = 4,
+    FEED_STATUS_INVALID_AMOUNT = 5
+};
+
+struct feed_request_t
+{
+    uint32_t timestamp;
+    uint8_t portions;
+    feed_source_t source;
+    QueueHandle_t reply_queue = nullptr; // nullptr = no reply needed
+};
+
+struct feed_status_t
+{
+    feed_status_code_t status_code;
+    uint8_t portions;
+    feed_source_t source;
+    uint32_t request_timestamp;
+};
+
+struct calibration_result_t
+{
+    float grams_per_portion;
+};
+
+struct schedule_data_t
+{
+    uint8_t hour;
+    uint8_t minute;
+    uint8_t portions;
+};
+
+struct visit_data_t
+{
+    uint32_t timestamp;
+    char name[32];
+};
+
+struct eating_data_t
+{
+    uint32_t timestamp;
+    uint8_t portions;
+};
+
+struct food_level_data_t
+{
+    uint8_t percentage;
+};
+
+struct wifi_event_data_t
+{
+    char ssid[33];
+};
+
+struct provisioning_event_data_t
+{
+    char device_name[33];
+};
+
+struct mqtt_message_data_t
+{
+    char topic[128];
+    char data[256];
+};
+
+#endif // TYPES_H
